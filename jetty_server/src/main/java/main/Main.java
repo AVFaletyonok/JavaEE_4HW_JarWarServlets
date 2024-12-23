c4d4464a515212946da6a93a66e20fb63a1b19a4
Alexander <AVFaletyonok@yandex.ru>
2024-12-24 02:35:52 +0300

    homeworks/homework02 : finish tomcat, jetty simple servers

---
+++ jetty_server/src/main/java/main/Main.java
@@ -0,0 +1,27 @@
+package main;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import servlets.AllRequestsServlet;
+
+/**
+ * @author A.Faletyonok
+ *         <p>
+ *         Project was downloaded from: https://github.com/AVFaletyonok/JavaEE_4HW_JarWarServlets
+ */
+public class Main {
+    public static void main(String[] args) throws Exception {
+        AllRequestsServlet allRequestsServlet = new AllRequestsServlet();
+
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.addServlet(new ServletHolder(allRequestsServlet), "/*");
+
+        Server server = new Server(8080);
+        server.setHandler(context);
+
+        server.start();
+        java.util.logging.Logger.getGlobal().info("Server started");
+        server.join();
+    }
+}
